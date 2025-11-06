ac8f9055dd548964dab0ce7d25d5d9b360561020
santhan <santhanpeddaguravagari@gmail.com>
2025-11-06 13:08:38 +0530

    created the first terraform script

---
+++ ec2/main.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_tls"
+  description = "Allow ssh inbound traffic and all outbound traffic"
+  
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  } 
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]  
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}  
+
