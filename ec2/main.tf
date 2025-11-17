e77d22614098381b4145a6205a3d9c31da0811b0
santhan <santhanpeddaguravagari@gmail.com>
2025-11-17 17:40:22 +0530

    terraform-ec2-practice

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,7 +1,8 @@
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_tls"
+resource "aws_security_group" "allow_ssh" { 
+  name        = "allow_ssh" 
   description = "Allow ssh inbound traffic and all outbound traffic"
   
+  # usually we allow everything in egress 
   egress {
     from_port        = 0
     to_port          = 0
@@ -10,9 +11,9 @@ resource "aws_security_group" "allow_ssh" {
   } 
 
   ingress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"  
     cidr_blocks      = ["0.0.0.0/0"]  
   }
 
@@ -20,4 +21,15 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_tls"
   }
 }   
+
+resource "aws_instance" "ec2-terraform" {
+  ami = "ami-09c813fb71547fc4f"  
+  instance_type = "t3.micro" 
+  vpc_security_group_ids = ["sg-07a395630b225807a"] # list means [] (box brackets) 
+
+  tags = {
+    Name = "ec2-terraform"  
+  }
+
+}
 
