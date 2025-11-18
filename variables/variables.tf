b9c9bea0733ceb0f12a4919eda2b504ab75b9b88
santhan <santhanpeddaguravagari@gmail.com>
2025-11-18 18:44:54 +0530

    terraform-variables-practice

---
+++ variables/variables.tf
@@ -0,0 +1,36 @@
+variable "sg_name" {
+    type = string
+    description = "this is the name of the security group"
+    default = "allow_ssh"
+}
+
+variable "ami_id" {
+    type = string
+    description = "this is ami id used by the ec2-instance"
+    default = "ami-09c813fb71547fc4f" 
+}
+
+variable "type_instance" {
+    type = string 
+    description = "type of the ec2-instance"
+    default = "t3.micro" 
+}
+
+
+variable "sg_tags" { 
+    description = "tags for the security group"
+    default = {
+        Name = "allow_tls"
+        Project = "expense" 
+        Terraform = "true" 
+    }
+}
+
+variable "ec2_tags" { 
+    description = "tags for the ec2-instance" 
+    default = {
+        Name = "ec2-terraform"  
+        Project = "expense" 
+        Terraform = "true" 
+    }
+}
